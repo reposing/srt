20cf1c9632211a8d6c2c4130a0e8c93ef35fe62b
Steve Norman <steve.norman@gmail.com>
2021-03-07 13:55:11 +0000

    Achievements page and menu restructure.

--- bin/riderHistory.js
+++ bin/riderHistory.js
@@ -33,6 +33,114 @@ async function TeamRiders() {
     }
 }
 
+async function BuildAchievments(achievements) {
+    const Handlebars = require("handlebars")
+
+    Handlebars.registerHelper('category', function (category) {
+        var categoryBadge = ''
+        switch (category) {
+            case 5:
+                categoryBadge = '<span class="badge label-cat-Aplus label-as-badge" style="font-size:8px;">A+</span>'
+                break
+            case 10:
+                categoryBadge = '<span class="badge label-cat-A label-as-badge" style="font-size:8px;">A</span>'
+                break
+            case 20:
+                categoryBadge = '<span class="badge label-cat-B label-as-badge" style="font-size:8px;">B</span>'
+                break
+            case 30:
+                categoryBadge = '<span class="badge label-cat-C label-as-badge" style="font-size:8px;">C</span>'
+                break
+            case 40:
+                categoryBadge = '<span class="badge label-cat-D label-as-badge" style="font-size:8px;">D</span>'
+                break
+            case 50:
+                categoryBadge = '<span class="badge label-cat-E label-as-badge" style="font-size:8px;">E</span>'
+                break
+        }
+        return new Handlebars.SafeString(categoryBadge)
+    })
+
+    Handlebars.registerHelper('isOdd', function (val, options) {
+        return val % 2 !== 0 ? options.fn(this) : options.inverse(this)
+    })
+
+    Handlebars.registerHelper('ISODate', function (date) {
+        var eventDate = new Date(date)
+        return eventDate.getFullYear() + "-" + (eventDate.getMonth() + 1).toString().padStart(2, '0') + "-" + eventDate.getDate().toString().padStart(2, '0')
+    })
+
+    Handlebars.registerHelper('eventType', function (eventType) {
+        var eventValue = ''
+        switch (eventType) {
+            case 'ftpBump':
+                eventValue = 'FTP Increase'
+                break
+            case 'mixedCategoryChange':
+                eventValue = 'Mixed Category Increase'
+                break
+            case 'womensCategoryChange':
+                eventValue = 'Womens Category Increase'
+                break
+            case 'riderJoined':
+                eventValue = 'Rider Joined'
+                break
+        }
+        return eventValue
+    })
+
+    Handlebars.registerHelper('eventDetails', function (eventType, eventData) {
+        var categoryBadge = function (category) {
+            var categoryBadge = ''
+            switch (category) {
+                case 5:
+                    categoryBadge = '<span class="badge label-cat-Aplus label-as-badge" style="font-size:8px;">A+</span>'
+                    break
+                case 10:
+                    categoryBadge = '<span class="badge label-cat-A label-as-badge" style="font-size:8px;">A</span>'
+                    break
+                case 20:
+                    categoryBadge = '<span class="badge label-cat-B label-as-badge" style="font-size:8px;">B</span>'
+                    break
+                case 30:
+                    categoryBadge = '<span class="badge label-cat-C label-as-badge" style="font-size:8px;">C</span>'
+                    break
+                case 40:
+                    categoryBadge = '<span class="badge label-cat-D label-as-badge" style="font-size:8px;">D</span>'
+                    break
+                case 50:
+                    categoryBadge = '<span class="badge label-cat-E label-as-badge" style="font-size:8px;">E</span>'
+                    break
+            }
+            return categoryBadge    
+        }
+
+        var eventValue = ''
+        switch (eventType) {
+            case 'ftpBump':
+                eventValue = `FTP increase from ${eventData.oldFTP} watts to ${eventData.newFTP} watts`
+                break
+            case 'mixedCategoryChange':
+                eventValue = `Mixed category increase from ${categoryBadge(eventData.oldCategory)} to ${categoryBadge(eventData.newCategory)}`
+                break
+            case 'womensCategoryChange':
+                eventValue = `Womens category increase from ${categoryBadge(eventData.oldCategory)} to ${categoryBadge(eventData.newCategory)}`
+                break
+            case 'riderJoined':
+                eventValue = 'Welcome to the team'
+                break
+        }
+        return new Handlebars.SafeString(eventValue)
+    })
+
+    templateContents = fs.readFileSync('templates/achievements.hbs')
+
+    var template = Handlebars.compile(templateContents.toString())
+    const teamsPage = template({ achievements: achievements })
+
+    fs.writeFileSync('site/achievements.html', teamsPage, 'utf8')
+}
+
 (async () => {
     const runDate = new Date()
     const riderHistoryPath = `data/riderHistory.json`
@@ -145,4 +253,8 @@ async function TeamRiders() {
 
     fs.writeFileSync(riderHistoryPath, JSON.stringify(riders, null, 2), 'utf8')
     fs.writeFileSync(honourRollPath, JSON.stringify(honourRoll, null, 2), 'utf8')
+
+    honourRoll.sort((a,b) => new Date(b.date) - new Date(a.date) )
+
+    await BuildAchievments(honourRoll)
 })()
