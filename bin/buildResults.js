3546e9bb3991889d9ab93c03c9d05f49a8d1cdfb
Steve Norman <steve.norman@gmail.com>
2021-03-15 13:50:53 +0000

    Add page for race counts for riders.

--- bin/buildResults.js
+++ bin/buildResults.js
@@ -180,6 +180,70 @@ async function BuildRaceResult(results, raceID, totalTeams, raceDate) {
     fs.writeFileSync(`site/results/ttt-${raceID}.html`, resultPage, 'utf8')
 }
 
+async function BuildRiderResults(riderSummary, raceCategories) {
+    const Handlebars = require('handlebars')
+
+    templateContents = fs.readFileSync('templates/raceRuns.hbs')
+
+    Handlebars.registerHelper('raceClass', function (raceClass) {
+        var classBadge = ''
+
+        switch (raceClass) {
+            case 'Espresso':
+                classBadge += '<span class="badge label-espresso">Espresso</span>'
+                break
+            case 'Frappe':
+                classBadge += '<span class="badge label-frappe">Frappe</span>'
+                break
+            case 'Latte':
+                classBadge += '<span class="badge label-latte">Latte</span>'
+                break
+            case 'Mocha':
+                classBadge += '<span class="badge label-mocha">Mocha</span>'
+                break
+            case 'Doppio':
+                classBadge += '<span class="badge label-vienna">Doppio</span>'
+                break
+            case 'Vienna':
+                classBadge += '<span class="badge label-vienna">Vienna</span>'
+                break
+            case 'Vienna-Frappe':
+                classBadge += '<span class="badge label-Fvienna">Vienna-Frappe</span>'
+                break
+            case 'Vienna-Latte':
+                classBadge += '<span class="badge label-Lvienna">Vienna-Latte</span>'
+                break
+            case 'Vienna-Espresso':
+                classBadge += '<span class="badge label-Evienna">Vienna-Espresso</span>'
+                break
+        }
+
+        return new Handlebars.SafeString(classBadge)
+    })
+
+    Handlebars.registerHelper('isOdd', function (val, options) {
+        return val % 2 !== 0 ? options.fn(this) : options.inverse(this)
+    })
+
+    Handlebars.registerHelper('categoryCount', function (races, category) {
+        var raceCount = 0
+        
+        var catCount = races.find(r => r.class === category) 
+
+        if (catCount) {
+            raceCount = catCount.count
+        }
+
+        return raceCount
+    })
+
+    var template = Handlebars.compile(templateContents.toString())
+    const resultPage = template({ riderSummary: riderSummary, raceCategories: raceCategories })
+
+    fs.writeFileSync(`site/riderRaces.html`, resultPage, 'utf8')
+}
+
+
 async function BuildRaceResultsSummary(result) {
     const Handlebars = require('handlebars')
 
@@ -216,14 +280,78 @@ function raceDate(raceID) {
     return raceDate.getFullYear() + "-" + (raceDate.getMonth() + 1).toString().padStart(2, '0') + "-" + raceDate.getDate().toString().padStart(2, '0')
 }
 
+async function RiderProfiles() {
+    const path = `data/profiles.json`
+    var profiles
+
+    try {
+        if (fs.existsSync(path)) {
+            profiles = JSON.parse(fs.readFileSync(path, 'utf8'))
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+    return profiles
+}
 
 (async () => {
     const resultSummary = []
+    const riderSummary = []
+    const raceCategories = []
+
+    const profiles = await RiderProfiles()
+
     for (i = 74; i <= 99; i++) {
         const results = await WTRLData(i)
         totalTeams = results.classes.reduce((a, b) => a + b.teamCount, 0)
         for (const result of results.teams) {
             result.a.sort((a, b) => a.bb - b.bb)
+
+            for (const rider of result.a) {
+                var riderRaces = riderSummary.find(r => r.profileId === rider.mm)
+                var existingClass = raceCategories.find(r => r === result.h)
+
+                if (!existingClass) {
+                    raceCategories.push(result.h)
+                }
+
+                if (riderRaces) {
+                    riderRaces.count++
+                    var raceCount = riderRaces.races.find(r => r.class === result.h)
+
+                    if (raceCount) {
+                        raceCount.count++
+                    } else {
+                        riderRaces.races.push({
+                            class: result.h,
+                            count: 1
+                        })
+                    }
+                } else {
+                    var profile = profiles.find(r => r.profileId === rider.mm)
+
+                    var riderName
+                    if (profile) {
+                        riderName = profile.name
+                    } else {
+                        riderName = rider.cc
+                    }
+
+                    riderRaces = {
+                        profileId: rider.mm,
+                        name: riderName,
+                        races: [{
+                            class: result.h,
+                            count: 1
+                        }],
+                        count: 1
+                    }
+
+                    riderSummary.push(riderRaces)
+                }
+
+            }
         }
 
         BuildRaceResult(results, i, totalTeams, raceDate(i))
@@ -233,5 +361,10 @@ function raceDate(raceID) {
         })
     }
 
-    BuildRaceResultsSummary(resultSummary.reverse())
+    await BuildRaceResultsSummary(resultSummary.reverse())
+
+    riderSummary.sort((a, b) => b.count - a.count)
+    raceCategories.sort((a, b) => a.localeCompare(b))
+
+    await BuildRiderResults(riderSummary, raceCategories)
 })()
