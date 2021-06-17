a4f12537f42b31487cca9d55480b3ad8ed68e654
Steve Norman <steve.norman@gmail.com>
2021-06-17 20:32:20 +0100

    Race day updates

--- bin/buildResults.js
+++ bin/buildResults.js
@@ -303,7 +303,7 @@ async function RiderProfiles() {
 
     const profiles = await RiderProfiles()
 
-    for (i = 74; i <= 112; i++) {
+    for (i = 74; i <= 113; i++) {
         const results = await WTRLData(i)
         totalTeams = results.classes.reduce((a, b) => a + b.teamCount, 0)
         for (const result of results.teams) {
