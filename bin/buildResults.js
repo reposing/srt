775bd655d301dc96d28b48151f1d93084f11ee6b
Steve Norman <steve.norman@gmail.com>
2021-09-09 20:03:33 +0100

    Early race results

--- bin/buildResults.js
+++ bin/buildResults.js
@@ -316,7 +316,7 @@ async function RiderProfiles() {
 
     const profiles = await RiderProfiles()
 
-    for (i = 74; i <= 124; i++) {
+    for (i = 74; i <= 125; i++) {
         const results = await WTRLData(i)
         totalTeams = results.classes.reduce((a, b) => a + b.teamCount, 0)
         for (const result of results.teams) {
